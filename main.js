682c14770d98dc52aa34e829332371d278ac52a5
Andrea Verlicchi <verlicchia@yoox.net>
2018-08-07 15:01:28 +0200

    `<script type="module">` + import

--- main.js
+++ main.js
@@ -1,25 +1,4 @@
 (function () {
-    function logEvent(eventName, element) {
-        console.log(Date.now(), eventName, element.getAttribute('data-src'), element.getAttribute('src'));
-    }
 
-    var llWebp = new LazyLoad({
-        elements_selector: ".lazy",
-        to_webp: true,
-
-        callback_enter: function (element) {
-            logEvent("ENTERED", element);
-        },
-        callback_load: function (element) {
-            logEvent("LOADED", element);
-        },
-        callback_set: function (element) {
-            logEvent("SET", element);
-        },
-        callback_error: function (element) {
-            logEvent("ERROR", element);
-            element.src = "https://placehold.it/220x280?text=Placeholder";
-        }
-    });
 
 }());
